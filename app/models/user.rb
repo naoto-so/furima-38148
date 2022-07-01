473e4c5328b469eb05cfe2546eef3a2c0824c43c
naoto <soe3.nao10@gmail.com>
2022-07-01 16:32:32 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :second_name, presence: true
+  validates :first_name, presence: true
+  validates :second_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :second_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :second_name_kana
+    validates :first_name_kana
+  end
+
 end
