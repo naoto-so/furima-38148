9e378c3aec94254b6879244bb11c847fc7bc7770
naoto <soe3.nao10@gmail.com>
2022-07-05 17:17:59 +0900

    重複したエラーメッセージの解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :date_of_birth, presence: true
   validates :second_name, presence: true
   validates :first_name, presence: true
   validates :second_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :date_of_birth, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, allow_blank: true do
     validates :second_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ }, allow_blank: true do
     validates :second_name_kana
     validates :first_name_kana
   end
