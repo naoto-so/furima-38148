12f77033bf88472a4674c1e7ab52dabfea58130c
naoto <soe3.nao10@gmail.com>
2022-07-10 16:55:58 +0900

    ordersテーブルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :send_area
   belongs_to :send_day
   has_one_attached :image
+  has_one :order
 
   validates :item_name,   presence: true
   validates :explanation, presence: true
